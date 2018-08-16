52ad5bdaeff625dc290ecfd02a3ba0ba8dfbac98
Christian Braybrooke <chris@purplemountmedia.com>
2018-08-16 15:35:52 +0100

    Update page

---
+++ source/_partials/footer.blade.php
@@ -0,0 +1,19 @@
+<footer>
+  <div class="mt-5 py-5 bg-primary border-top border-secondary">
+    <div class="container">
+      <div class="row text-light">
+        <div class="col">
+
+          <h4>Site Navigation</h4>
+
+        </div>
+
+        <div class="col">
+
+          <h4>Contact Us</h4>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
